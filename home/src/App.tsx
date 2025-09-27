68d65c08da40dc56096a62e8cfbe4d0a73c1b355
DariusETH <ceqedcmgic4379@hotmail.com>
2025-09-27 19:03:21 +0800

    create first app

---
+++ home/src/App.tsx
@@ -0,0 +1,22 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { config } from './config/wagmi';
+import { SafePollApp } from './components/SafePollApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <SafePollApp />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App;
