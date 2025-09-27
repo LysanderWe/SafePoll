9128c3e19b5ad565a0d842592cfc0604cdbe3044
LysanderWe <uvyvrujn341@hotmail.com>
2025-09-27 19:02:15 +0800

    deploy safepoll

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedSafePoll = await deploy("SafePoll", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`SafePoll contract: `, deployedSafePoll.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_safePoll"; // id required to prevent reexecution
+func.tags = ["SafePoll"];
